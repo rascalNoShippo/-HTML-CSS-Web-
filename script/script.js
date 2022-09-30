b673f79c7a6437803282ccd47d4ed11f8fee6aab
rascalnoshippo <naofirefox@gmail.com>
2022-09-30 20:50:25 +0900

    Adjust animation

--- script/script.js
+++ script/script.js
@@ -1,13 +1,26 @@
 $(function() {
     $(".hbg_menu, .menu a").on("click", function(event){
-        $(".hbg_menu").toggleClass("active");
-        $(".menu").fadeToggle();
+        $(".hbg_menu, .menu").toggleClass("active");
+        
+        //$(".menu").fadeToggle();
         event.preventDefault(); 
     });
 });
 
-$(function() {
+$(function(){ //smooth scrolling
+    $('a[href^="#"]').click(function(){
+      let adjust = -50;
+      let speed = 400;
+      let href= $(this).attr("href");
+      let target = $(href == "#" || href == "" ? 'html' : href);
+      let position = target.offset().top + adjust;
+      $('body,html').animate({scrollTop:position}, speed, 'swing');
+      return false;
+    });
+  });
+
+/*$(function() {
     $(".menu a").on("click", function(){
         location.href = $(this).attr("href");
     });
-});
+});*/
