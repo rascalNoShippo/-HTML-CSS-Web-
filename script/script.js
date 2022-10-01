e0b548fd5554abb35bf7a3476f531812d727f821
rascalnoshippo <naofirefox@gmail.com>
2022-10-01 14:08:47 +0900

    Adjusting properties of overflow and the TOP button

--- script/script.js
+++ script/script.js
@@ -1,26 +1,52 @@
 $(function() {
-    $(".hbg_menu, .menu a").on("click", function(event){
-        $(".hbg_menu, .menu").toggleClass("active");
-        
-        //$(".menu").fadeToggle();
-        event.preventDefault(); 
-    });
+	$(".hbg_menu, .menu a").on("click", function(event){
+		$(".hbg_menu, .menu").toggleClass("active");
+		event.preventDefault(); 
+	});
 });
 
 $(function(){ //smooth scrolling
-    $('a[href^="#"]').on("click", function(){
-      let adjust = -50;
-      let speed = 400;
-      let href= $(this).attr("href");
-      let target = $(href == "#" || href == "" ? 'html' : href);
-      let position = target.offset().top + adjust;
-      $('body,html').animate({scrollTop:position}, speed, 'swing');
-      return false;
-    });
-  });
+	$('a[href^="#"]').on("click", function(){
+		let adjust = 0;
+		let speed = 400;
+		let href= $(this).attr("href");
+		let target = (href == "#" || href == "" ? 0 : $(href).offset().top - $("#main_top").offset().top);
+		let position = target + adjust;
+		$('main').animate({scrollTop:position}, speed, 'swing');
+		return false;
+	});
+});
+
+let appear = false;
+
+$(function(){
+	if($("main").scrollTop() > 100){
+		$("#top_btn").css({"bottom":"10px"});
+		appear = true;
+	}
+});
+
+$(function(){
+	$("main").scroll(function(){
+		if($("main").scrollTop() > 100){
+			appear = true;
+			$("#top_btn").css({"bottom":"10px"});
+		}else{
+			appear = false;
+			$("#top_btn").css({"bottom":"-60px"});
+		}
+	});
+});
+
+$(function(){ // go top button action
+	$("#top_btn").on("click", function(){
+		let speed = 400;
+		$('main').animate({scrollTop:0}, speed, 'swing');
+	})
+});
 
 /*$(function() {
-    $(".menu a").on("click", function(){
-        location.href = $(this).attr("href");
-    });
+	$(".menu a").on("click", function(){
+		location.href = $(this).attr("href");
+	});
 });*/
